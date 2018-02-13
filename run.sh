524932620199f33e81cf02f21ee52ee08437fe7d
Raju Rimal <therimalaya@gmail.com>
2018-02-13 09:58:06 +0100

    getting stared realesed

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-Rscript -e "bookdown::render_book('.')"
+Rscript -e "bookdown::render_book('.', output_format = 'bookdown::pdf_book')"
+Rscript -e "bookdown::render_book('.', output_format = 'bookdown::gitbook')"
+Rscript -e "bookdown::render_book('.', output_format = 'bookdown::epub_book')"
